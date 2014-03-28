b022998b142575e8f56cdd600860f756039f9615
k2r79 <k2r79@hotmail.fr>
2014-03-28 12:19:52 +0100

    Project Structure

---
+++ src/main/java/fizzbuzz/Test.java
@@ -0,0 +1,12 @@
+package fizzbuzz;
+
+/**
+ * Created by Vincent on 28/03/2014.
+ */
+public class Test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello !");
+    }
+}
