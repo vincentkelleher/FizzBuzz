8bcc745e9d98203c9736bffdd147d7d60605fd19
k2r79 <k2r79@hotmail.fr>
2014-04-11 12:38:22 +0200

    Added Configuration files + 8080 Port

--- src/main/java/com/WebDisplay.java
+++ src/main/java/com/WebDisplay.java
@@ -6,11 +6,12 @@ import spark.Response;
 import spark.Route;
 
 import static spark.Spark.get;
+import static spark.Spark.setPort;
 
 public class WebDisplay
 {
     public static void main(String[] args) {
-
+        setPort(8080);
         get(new Route("/fizzbuzz/:number") {
             @Override
             public Object handle(Request request, Response response) {
